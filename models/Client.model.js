f017173507c44986333d3ef8c28e26beb054c2a7
Diego Cisneros <dcisnerosg28@gmail.com>
2024-12-03 11:00:54 +0100

    data-models

---
+++ models/Client.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const clientSchema = mongoose ({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required."],
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    company:{
+        type: String,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", // Reference to the User model
+        required: true,
+    },
+})
+
+const Client = mongoose.model("Client", clientSchema);
+
+module.exports = Client;
+
---
+++ models/Project.model.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const projectSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Title is required"],
+            trim: true,
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        budget: {
+            type: Number,
+            required: true,
+            min: [0, "Budget cannot be negative"],
+        },
+        status: {
+            type: String,
+            enum: ["In Progress", "Completed", "On Hold"], 
+            default: "In Progress",
+        },
+        client: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Client", 
+            required: true,
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User", 
+            required: true,
+        },
+    },
+    {
+        timestamps: true, 
+    }
+);
+
+const Project = mongoose.model("Project", projectSchema);
+
+module.exports = Project;
